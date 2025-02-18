d5da0142f6500000cec9c9c335fd8b3858d02e48
justine <jg2559973@gmail.com>
2025-02-18 14:29:41 +0800

    Submitting Python Week 04 - Laboratory # 14

--- nested_for_loop_multiplication_table.py
+++ nested_for_loop_multiplication_table.py
@@ -1,13 +1,9 @@
-nested_for_loop_multiplication_table.py
+ nested_for_loop_multiplication_table.py
 
-#Outer loop for rows (1 to 5):
-for i in range(1, 6):
-#Inner loop for columns (1 to 5): 
-    for j in range(1, 6):  # Indent this loop!
-	#Calculate the product
-	        product = i * j  # Indent this line!
-			#Print the product with formatting
-			        print(f"{product:4}", end="")  # Indent this line!
-					#New line after each row
-					    print() # Indent this line!
-						
+# Outer loop for rows (1 to 5)
+for i in range(1, 6):  
+    # Inner loop for columns (1 to 5)
+    for j in range(1, 6):  
+        product = i * j  # Calculate the product of i and j
+        print(f"{product:4}", end="")  # Print the product with formatting
+    print()  # New line after each row
